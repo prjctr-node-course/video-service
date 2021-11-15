1a938f2be3e9e1b260fddffe8c5cf013a33f0f2b
nazzarkevich <nazarjtk@gmail.com>
2021-11-15 12:12:37 +0200

    fix upload validation, error handling

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const STATUS_CODES = {
   success: 200,
   notFound: 404,
   notAllowed: 405,
+  unsupportedMedia: 415,
 };
 
 const REQUEST_METHODS = {
@@ -42,28 +43,66 @@ function getFileType(contentType) {
   return contentType.split("/")[1];
 }
 
-function uploadRoute() {
-  return {
-    response: "Upload route",
-    code: STATUS_CODES.success,
-  };
-}
-
-function notFoundRoute() {
-  return {
-    response: "Not found",
-    code: STATUS_CODES.notFound,
-  };
-}
-
-function resolveRoutes(pathname) {
+function resolvePostRoutes(req, res, fileDetails, pathname) {
   switch (pathname) {
     case ROUTES.upload:
-      return uploadRoute();
+      uploadRoute(req, res, fileDetails);
+      break;
 
     default:
-      return notFoundRoute();
+      notFoundRoute(res);
+      break;
   }
+}
+
+async function uploadRoute(req, res, fileDetails) {
+  await handleFileDownload(req, fileDetails.filePath);
+  await convertAndDeleteVideo(res, fileDetails);
+}
+
+function notFoundRoute(res) {
+  res.statusCode = STATUS_CODES.notFound;
+  res.end("Not found");
+}
+
+function removeFile(filePath) {
+  fs.unlink(filePath, (err) => {
+    if (err) throw err;
+
+    console.log("File deleted!");
+  });
+}
+
+function convertAndDeleteVideo(res, fileDetails) {
+  hbjs
+    .spawn({
+      input: fileDetails.fileName,
+      output: `${fileDetails.fileHash}.mp4`,
+    })
+    .on("error", () => {
+      removeFile(fileDetails.filePath);
+    })
+    .on("end", () => {
+      removeFile(fileDetails.filePath);
+
+      res.statusCode = STATUS_CODES.success;
+      res.end("Upload route");
+    });
+}
+
+async function handleFileDownload(req, filePath) {
+  const videoFile = fs.createWriteStream(filePath);
+
+  await new Promise((resolve, reject) => {
+    req
+      .pipe(videoFile)
+      .on("error", (error) => {
+        reject(error);
+      })
+      .on("finish", () => {
+        resolve();
+      });
+  });
 }
 
 const server = http.createServer((req, res) => {
@@ -73,46 +112,39 @@ const server = http.createServer((req, res) => {
   const fileType = getFileType(req.headers["content-type"]);
   const fileHash = `${Date.now()}${getUUID()}`;
   const fileName = `${fileHash}.${fileTypes[fileType]}`;
-  const filePath = path.join(__dirname, fileName);
-  const videoFile = fs.createWriteStream(filePath);
+
+  const fileDetails = {
+    fileHash,
+    fileName,
+    filePath: path.join(__dirname, fileName),
+  };
 
   const isFileTypeAllowed = Object.keys(fileTypes).includes(fileType);
 
-  function convertAndDeleteVideo() {
-    const resolvedDetails = resolveRoutes(pathname);
-
-    hbjs
-      .spawn({
-        input: fileName,
-        output: `${fileHash}.mp4`,
-      })
-      .on("end", () => {
-        fs.unlink(filePath, (err) => {
-          if (err) throw err;
-
-          console.log("File deleted!");
-        });
-
-        res.statusCode = resolvedDetails.code;
-        res.end(resolvedDetails.response);
-      });
+  function resolveStatus(status, msg) {
+    res.statusCode = status;
+    res.end(msg);
   }
 
-  function handleFileDownload() {
-    req.pipe(videoFile);
-  }
+  switch (method) {
+    case REQUEST_METHODS.post:
+      if (!isFileTypeAllowed) {
+        resolveStatus(STATUS_CODES.unsupportedMedia, "Unsupported Media Type");
 
-  if (method === REQUEST_METHODS.post) {
-    if (isFileTypeAllowed) {
-      handleFileDownload();
-      convertAndDeleteVideo();
-    } else {
-      res.statusCode = 415;
-      res.end("Unsupported Media Type");
-    }
-  } else {
-    res.statusCode = 405;
-    res.end("Method Not Allowed");
+        return;
+      }
+
+      resolvePostRoutes(req, res, fileDetails, pathname);
+      break;
+
+    case REQUEST_METHODS.get:
+    case REQUEST_METHODS.delete:
+      resolveStatus(STATUS_CODES.notAllowed, "Method Not Allowed");
+      break;
+
+    default:
+      resolveStatus(STATUS_CODES.notAllowed, "Method Not Allowed");
+      break;
   }
 });
 
