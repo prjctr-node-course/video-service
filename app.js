35068e60a6fb46063fa9c8df35c865c8707774b9
nazzarkevich <nazarjtk@gmail.com>
2021-11-27 18:29:53 +0200

    fix app

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-const http = require("http");
-const url = require("url");
-const fs = require("fs");
-const crypto = require("crypto");
-const path = require("path");
-const hbjs = require("handbrake-js");
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const crypto = require('crypto');
+const path = require('path');
+const hbjs = require('handbrake-js');
 
-const hostname = "127.0.0.1";
+const hostname = '127.0.0.1';
+
 const port = 3000;
 const ROUTES = {
-  main: "/",
-  upload: "/upload",
+  main: '/',
+  upload: '/upload',
 };
 
 const STATUS_CODES = {
@@ -20,27 +21,27 @@ const STATUS_CODES = {
 };
 
 const REQUEST_METHODS = {
-  get: "GET",
-  post: "POST",
-  delete: "DELETE",
+  get: 'GET',
+  post: 'POST',
+  delete: 'DELETE',
 };
 
 const fileTypes = {
-  "x-msvideo": "avi",
-  quicktime: "mov",
-  mp4: ".mp4",
+  'x-msvideo': 'avi',
+  quicktime: 'mov',
+  mp4: '.mp4',
 };
 
 function getUUID() {
-  return crypto.randomBytes(16).toString("hex");
+  return crypto.randomBytes(16).toString('hex');
 }
 
 function getFileType(contentType) {
   if (!contentType) {
-    return "";
+    return '';
   }
 
-  return contentType.split("/")[1];
+  return contentType.split('/')[1];
 }
 
 function resolvePostRoutes(req, res, fileDetails, pathname) {
@@ -62,14 +63,14 @@ async function uploadRoute(req, res, fileDetails) {
 
 function notFoundRoute(res) {
   res.statusCode = STATUS_CODES.notFound;
-  res.end("Not found");
+  res.end('Not found');
 }
 
 function removeFile(filePath) {
   fs.unlink(filePath, (err) => {
     if (err) throw err;
 
-    console.log("File deleted!");
+    console.log('File deleted!');
   });
 }
 
@@ -79,14 +80,14 @@ function convertAndDeleteVideo(res, fileDetails) {
       input: fileDetails.fileName,
       output: `${fileDetails.fileHash}.mp4`,
     })
-    .on("error", () => {
+    .on('error', () => {
       removeFile(fileDetails.filePath);
     })
-    .on("end", () => {
+    .on('end', () => {
       removeFile(fileDetails.filePath);
 
       res.statusCode = STATUS_CODES.success;
-      res.end("Upload route");
+      res.end('Upload route');
     });
 }
 
@@ -96,10 +97,10 @@ async function handleFileDownload(req, filePath) {
   await new Promise((resolve, reject) => {
     req
       .pipe(videoFile)
-      .on("error", (error) => {
+      .on('error', (error) => {
         reject(error);
       })
-      .on("finish", () => {
+      .on('finish', () => {
         resolve();
       });
   });
@@ -109,7 +110,7 @@ const server = http.createServer((req, res) => {
   const pathname = url.parse(req.url, false).pathname;
   const method = req.method;
 
-  const fileType = getFileType(req.headers["content-type"]);
+  const fileType = getFileType(req.headers['content-type']);
   const fileHash = `${Date.now()}${getUUID()}`;
   const fileName = `${fileHash}.${fileTypes[fileType]}`;
 
@@ -129,7 +130,7 @@ const server = http.createServer((req, res) => {
   switch (method) {
     case REQUEST_METHODS.post:
       if (!isFileTypeAllowed) {
-        resolveStatus(STATUS_CODES.unsupportedMedia, "Unsupported Media Type");
+        resolveStatus(STATUS_CODES.unsupportedMedia, 'Unsupported Media Type');
 
         return;
       }
@@ -139,11 +140,11 @@ const server = http.createServer((req, res) => {
 
     case REQUEST_METHODS.get:
     case REQUEST_METHODS.delete:
-      resolveStatus(STATUS_CODES.notAllowed, "Method Not Allowed");
+      resolveStatus(STATUS_CODES.notAllowed, 'Method Not Allowed');
       break;
 
     default:
-      resolveStatus(STATUS_CODES.notAllowed, "Method Not Allowed");
+      resolveStatus(STATUS_CODES.notAllowed, 'Method Not Allowed');
       break;
   }
 });
